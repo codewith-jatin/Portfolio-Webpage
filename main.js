9c26416a8e0c8bb3ec5bba08966fc4e9d0dce109
Jatin Gupta <144834913+codewith-jatin@users.noreply.github.com>
2023-12-07 10:04:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// var typed= new typed(".text", {
+//     Strings:["Frontend Developer" , "Web Developer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
+
+var typed = new Typed('.text', {
+    strings: [
+      'Frontend Developer' , 'Web Developer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 80,
+    loop: true
+  });
